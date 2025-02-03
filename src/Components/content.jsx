aee70d96c16d176c3c9efc6237244fceb74b4386
Vijesh120503 <94106218+Vijesh120503@users.noreply.github.com>
2025-02-03 19:56:41 +0530

    Update content.jsx

--- src/Components/content.jsx
+++ src/Components/content.jsx
@@ -247,10 +247,10 @@ const Heros = ({ onNavClick,onSongChange, onAudioChange }) => {
         const matchesFromFifthJson = data5.fixtures.flatMap((fixture) => {
          return fixture.streams
      .filter((stream) => 
-       // Ensure streamName exists before calling toLowerCase()/  
+       // Ensure streamName exists before calling toLowerCase() 
       const containsCricket = stream.streamName?.toLowerCase().includes("cricket");
-       const isUpcomingOrLive = stream.status === "UPCOMING" || stream.status === "LIVE";
-       return containsCricket && isUpcomingOrLive;
+      const isUpcomingOrLive = stream.status === "UPCOMING" || stream.status === "LIVE";
+        return containsCricket && isUpcomingOrLive;
      })
     .map((stream) => {
        return {
