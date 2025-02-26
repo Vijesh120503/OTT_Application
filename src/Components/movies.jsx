2fcac88780c2f80e93764ad3b80ab7c632c5d191
Vijesh120503 <94106218+Vijesh120503@users.noreply.github.com>
2025-02-26 22:38:38 +0530

    Update movies.jsx

--- src/Components/movies.jsx
+++ src/Components/movies.jsx
@@ -9,7 +9,7 @@ const shows = [
     name: 'Theatre Print',
     image: 'https://s2.r29static.com/bin/entry/2ab/0,791,5615,2948/x,80/1650643/image.jpg',
     shows: [
-        {name:'Neek',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY5SY-sePmPC0EbIoPKyBYSPPgL4Og3ca37DmreY7m5QeiH0SwoinuH1Q&s=10',link:'https://dls3.mrfooll.xyz/AgADbi3684453'},
+        {name:'Neek',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY5SY-sePmPC0EbIoPKyBYSPPgL4Og3ca37DmreY7m5QeiH0SwoinuH1Q&s=10',link:'https://pub-18418916b5a34df9a73eee4ebcfb4b64.r2.dev/Nilavuku%20Enmel%20Ennadi%20Kobam%20(2025)%20Tamil%20HQ%20PreDVD%20-%20x264%20-%20HQ%20Clean%20Audio%20-%20AAC%20-%20700MB.mkv'},
         {name:'Dragon',image:'https://assetscdn1.paytm.com/images/cinema/Dragon-galery-08d794a0-cc0a-11ef-912e-2154ccd78c0a.jpg',link:'https://pub-18418916b5a34df9a73eee4ebcfb4b64.r2.dev/Dragon%20(2025)%20Tamil%20HQ%20PreDVD%20-%20x264%20-%20HQ%20Clean%20Audio%20-%20AAC%20-%20700MB.mkv'},
  {name:'Captain America',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUJsXek9A0mAYunJEITCnwbCjVzf8tugDQENOsxbOVlGa_JmGFu4kMSkU&s=10',link:'https%3A%2F%2Fs07.dubshare.one%2FCaptain_America_Brave_New_World_2025_360p_PreDVD.mp4'},
 {name:'Thandel',image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRppQKLkGTsxaPv9ECnI3dm27xFsxI9mxJCPuN334N1v3Dc3tO7ImiFmfx&s=10',link:'https://pub-18418916b5a34df9a73eee4ebcfb4b64.r2.dev/Vidaamuyarchi%20(2025)%20Tamil%20v2%20HDRip%20-%20x264%20-%20(AAC%202.0)%20-%20700MB%20-%20HC-ESub.mkv'},
